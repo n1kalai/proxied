94115bf4f4dadac8ceaad17730567f2cad099d51
Nikoloz Palagashvili <nikoloz.palagashvili@gmail.com>
2025-02-19 19:22:25 +0400

    Added basic apolloClient configuration with getproducts query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import { GET_PRODUCTS_QUERY} from "@/services/get-data";
+import { client } from "./lib/apollo-client";
+
+export default async function Home() {
+
+const data =  client.query({query: GET_PRODUCTS_QUERY})
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
