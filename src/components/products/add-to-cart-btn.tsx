47c19e9a9501e7c32d7754b7b3f4e72c823b5f44
Nikoloz Palagashvili <nikoloz.palagashvili@gmail.com>
2025-02-20 21:26:03 +0400

    added navigate to cart button to toast

--- src/components/products/add-to-cart-btn.tsx
+++ src/components/products/add-to-cart-btn.tsx
@@ -3,6 +3,8 @@ import { useMutation } from '@apollo/client';
 import { Button } from '../ui/button';
 import { AddToCartIcon } from '../icons/add-to-cart';
 import { useToast } from '@/hooks/use-toast';
+import { ToastAction } from '../ui/toast';
+import Link from 'next/link';
 
 export const AddToCartBtn = ({ productId }: { productId: string }) => {
   const [addItem] = useMutation(ADD_CART_ITEM_MUTATION);
@@ -21,6 +23,11 @@ export const AddToCartBtn = ({ productId }: { productId: string }) => {
           toast({
             title: 'Added to cart',
             description: 'Item added to cart successfully.',
+            action: (
+              <ToastAction altText="Goto schedule to undo">
+                <Link href="/cart">View Cart</Link>
+              </ToastAction>
+            ),
           });
         },
 
