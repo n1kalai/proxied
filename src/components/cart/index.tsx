884e60c3d7ab57e0b835f9c6d901ad55ae55626d
Nikoloz Palagashvili <nikoloz.palagashvili@gmail.com>
2025-02-20 13:01:49 +0400

    changed cart item card at cart page

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,28 +1,30 @@
-"use client"
+'use client';
 
-import { GET_CART_QUERY } from "@/services/queries/get-cart-query";
-import { CartResponseType } from "@/types/cart/cart-type";
-import { useQuery } from "@apollo/client";
+import { GET_CART_QUERY } from '@/services/queries/get-cart-query';
+import { CartResponseType } from '@/types/cart/cart-type';
+import { useQuery } from '@apollo/client';
+import { ProductCard } from '../products/product-card';
 
 export function Cart() {
   const { data, loading, error } = useQuery<CartResponseType>(GET_CART_QUERY, {
-    fetchPolicy: "network-only", // Always fetch fresh data
+    fetchPolicy: 'network-only', // Always fetch fresh data
   });
-  
 
   if (loading) return <p>Loading cart...</p>;
   if (error) return <p>Error loading cart</p>;
-  
-  if(!data?.getCart?.items?.length) return <p>No cart items</p>;
+
+  if (!data?.getCart?.items?.length) return <p>No cart items</p>;
 
   return (
     <div>
       {data.getCart.items.map((item) => (
-        <div key={item._id}>
-          {item.product.title} - {item.quantity}
-        </div>
+        <ProductCard
+          key={item._id}
+          cost={item.product.cost}
+          title={item.product.title}
+          id={item.product._id}
+        />
       ))}
-      
     </div>
   );
 }
