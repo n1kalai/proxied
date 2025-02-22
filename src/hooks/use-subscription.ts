8abc309b1e455b3cc89393015041941ac0590a29
Nikoloz Palagashvili <nikoloz.palagashvili@gmail.com>
2025-02-22 13:27:50 +0400

    added types to use cart subscription and removed unnecessary logs from apollo client

--- src/hooks/use-subscription.ts
+++ src/hooks/use-subscription.ts
@@ -1,7 +1,25 @@
 import { useSubscription } from '@apollo/client';
 import { CART_ITEM_SUBSCRIPTION } from '@/graphql/subscriptions/cart-item-subscription';
 
+import { ProductType } from '@/types/product-type';
+
+export type Payload = {
+  _id: string;
+  quantity: number;
+  isOut?: boolean;
+  product: ProductType;
+};
+
+type SocketResponse = {
+  cartItemUpdate: {
+    event: string;
+    payload: Payload;
+  };
+};
+
 export const useCartSubscription = () => {
-  const { data, loading, error } = useSubscription(CART_ITEM_SUBSCRIPTION);
+  const { data, loading, error } = useSubscription<SocketResponse>(
+    CART_ITEM_SUBSCRIPTION,
+  );
   return { data, loading, error };
 };
